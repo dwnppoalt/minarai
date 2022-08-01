ed6a5e2b7520777b86a992b07ca14ab2cd965939
dwnppoalt <95208803+dwnppoalt@users.noreply.github.com>
2022-08-01 09:57:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,16 +3,18 @@ from cogs.cogs import MainCogs
 from discord.ext import commands
 import os
 TOKEN = os.environ["TOKEN"]
-activity = discord.Activity(type=discord.ActivityType.listening, name="?help")
+activity = discord.Activity(type=discord.ActivityType.listening, name=">help")
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='?', help_command=None, intents=intents, activity=activity, status=discord.Status.online)
+bot = commands.Bot(command_prefix='>', help_command=None, intents=intents, activity=activity, status=discord.Status.online)
 
 @bot.event
 async def on_ready():
-    await bot.add_cog(MainCogs(bot))
+    bot.add_cog(Music(bot))
+    bot.add_cog(MainCogs(bot))
     print('{:^43}'.format('Logged in as'))
     print("{:^20} | {:^20}".format("Username", "User ID"))
     print("{:^20} | {:^20}".format(bot.user.name, bot.user.id))
 
 bot.run(TOKEN)
+
