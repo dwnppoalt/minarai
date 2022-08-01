8f8a6807396737a41ec2edab58b221a00ac290d4
dwnppoalt <95208803+dwnppoalt@users.noreply.github.com>
2022-08-01 10:01:50 +0800

    Delete proofreader.py

--- cogs/apis/proofreader.py
+++
@@ -1,8 +0,0 @@
-from fastpunct import FastPunct
-
-class Proofreader:
-    def __init__(self) -> None:
-        self.fastpunct = FastPunct()
-    
-    def proofread(self, query):
-        return self.fastpunct.punct(query, correct=True)
